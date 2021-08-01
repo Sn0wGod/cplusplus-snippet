2379ccd91e801038c144eee7ae33bcaca8e31f82
Yogesh Yogendra <53307155+Sn0wGod@users.noreply.github.com>
2021-08-01 12:06:11 +0530

    Update snippet.cpp

--- snippet.cpp
+++ snippet.cpp
@@ -14,48 +14,15 @@ void dfile() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 }
-stack<int> st;
 
-void insert_at_bottom(char x)
-{
-    if(st.size() == 0)
-    st.push(x);
- 
-    else
-    {
-        char a = st.top();
-        st.pop();
-        insert_at_bottom(x);
-        st.push(a);
-    }
-}
-void reverse()
-{
-    if(st.size()>0)
-    {
-        char x = st.top();
-        st.pop();
-        reverse();
-        insert_at_bottom(x);
-    }
-}
+
 
 int32_t main() {
   
      
    dfile();
     
-    st.push(1);
-    st.push(2);
-    st.push(3);
-    st.push(4);
-
-    reverse();
-
-    while(!st.empty()){
-        cout<<st.top()<<endl;
-        st.pop();
-    }
+  
 
     return 0;
 }
